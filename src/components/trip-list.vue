17ea999945b1f81a00c9b4d68fd376aef382ac21
yairharaz <yairharaz@gmail.com>
2020-07-16 16:09:58 +0300

    list

---
+++ src/components/trip-list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="experience-list">
+    <h1>experience List</h1>
+    <router-link to="/experience/edit/"><span>Add Activity</span></router-link>
+    <ul>
+      <experience-preview v-for="experience in experiences" :key="experience._id" :experience="experience"></experience-preview> 
+    </ul>
+  </div>
+</template>
+
+<script>
+import experiencePreview from '../components/experience-preview.cmp';
+
+export default {
+    props:['experiences'],
+    data(){
+        return {
+
+        }
+    },
+    components: {
+        experiencePreview
+    }
+}
+</script>
