a2ead9f2ee07a0f7fbfbbf62181a21d331802681
raniel <rbhrbh65@gmail.com>
2020-07-18 15:16:14 +0300

    add book

--- src/components/exp-book.vue
+++ src/components/exp-book.vue
@@ -1,12 +1,48 @@
+
 <template>
-  <section>
-    Book
-  </section>
+  <div class="exp-book">
+    <!-- <h2>Choose</h2> -->
+    <date-picker @setDay="setDay" />
+     <h2>Number Of Tickets</h2>
+    <select-capacity :participants="exp.participants"  :capacity="exp.capacity" @setTicket="setTicket"/>
+    <p>{{bookPrice}}</p>
+    <button @click="book">Really your dream</button>
+  </div>
 </template>
 
 <script>
-export default{
-    
+import datePicker from './date-picker.vue';
+import selectCapacity from './select-capacity.vue';
+export default {
+  props: ['exp'],
+   data() {
+      return {
+        dayPicker: null,
+        numOfTicket: 1
+      }
+    },
+    methods:{
+      book(){
+        if(!this.dayPicker) return
+        console.log('numOfTicket: ' , this.numOfTicket ,'DAYPICKER: ' ,this.dayPicker)
+      },
+      setDay(day){
+        this.dayPicker = day
+        // this.dayPicker = day.getTime();
+        console.log(this.dayPicker)
+      },
+      setTicket(numOfTicket){
+        this.numOfTicket = numOfTicket;
+      }
+    },
+    computed: {
+      bookPrice(){
+         return '$' + this.exp.currPrice * this.numOfTicket 
+      }
+    },
+    components: {
+       datePicker,
+       selectCapacity
+   },
 }
-
 </script>
---
+++ src/components/select-capacity.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-select v-model="value" placeholder="1">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+      :disabled="item.disabled">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+  export default {
+    props: ['participants' , 'capacity'],
+    data() {
+      return {
+        options: [{
+          value: 1,
+          label: '1',
+          disabled: false
+        }, {
+          value: 2,
+          label: '2',
+          disabled: false
+        }, {
+          value: 3,
+          label: '3',
+           disabled: false
+        }, {
+          value: 4,
+          label: '4',
+          disabled: false
+        }, {
+          value: 5,
+          label: '5',
+          disabled: false
+        }],
+        value: 1,
+      }
+    },
+    created(){
+      const numberOfSoldTickets = this.participants.reduce((acc , participant) => {
+          return acc + participant.numOfTicket
+      }, 0);
+      const numberOfAvailableTickets =  this.capacity - numberOfSoldTickets;
+      this.options = this.options.map((option) => {
+        option.disabled  = (option.value > numberOfAvailableTickets) ? true : false;
+        return option
+      })
+    },
+     watch: {
+     value(){
+        this.$emit('setTicket' , this.value)
+     }
+  },
+}
+</script>
