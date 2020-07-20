4f7c883bda9e16bef4eff4a16a601dfc7f5a528b
Palachy <Oren.Palachy@lamresearch.com>
2020-07-20 21:44:38 +0300

    none

--- src/views/user-login.vue
+++ src/views/user-login.vue
@@ -1,15 +1,6 @@
 <template>
     <section class="user-login">
-<<<<<<< HEAD
-      <h1>Login:</h1>
-      <div v-if="loggedinUser">
-          <h3>Currently logged as: {{loggedinUser.userName}}</h3>
-          <button @click="logout">Logout</button>
-      </div>
-      <form v-else @submit.prevent="login">
-=======
       <form @submit.prevent="login">
->>>>>>> 36764a113861bcc1494b73662c3cf111bc803c6a
         <input placeholder="Please input user name" v-model="credentials.userName"/>
         <input placeholder="Please input password" v-model="credentials.password" show-password/>
         <button>Login</button>
@@ -31,32 +22,12 @@ export default {
     }
   },
   methods: {
-<<<<<<< HEAD
-     async login() {
-      const user = await this.$store.dispatch({type: 'login', userCred: this.credentials})
-      // console.log('loggedin user:', this.loggedinUser)
-      this.loadLoggedinUser()
-      console.log('user-login: ',this.loggedinUser);
-      this.$router.push('/')
-    },
-    async logout(){
-      let user = await this.$store.dispatch({type: 'logout'})
-      console.log('loggedout user:', user);
-      // this.loadLoggedinUser()
-=======
      login() {
       this.$store.dispatch({type: 'login', userCred: this.credentials})
->>>>>>> 36764a113861bcc1494b73662c3cf111bc803c6a
       this.$router.push('/')
     },
     signup(){
         this.$router.push('/signup')
-<<<<<<< HEAD
-    },
-    loadLoggedinUser(){
-      this.loggedinUser = this.$store.getters.loggedinUser
-=======
->>>>>>> 36764a113861bcc1494b73662c3cf111bc803c6a
     }
   },
   created() {
