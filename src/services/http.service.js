7dcea887be31bc473a0a276da6cb7c3b8c25bb33
Palachy <Oren.Palachy@lamresearch.com>
2020-07-26 15:13:54 +0300

    nnn

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,8 +1,8 @@
 import router from '@/router'
 
-const BASE_URL = process.env.NODE_ENV === 'production'
+const BASE_URL = process.env.NODE_ENV !== 'development'
     ? '/api/'
-    : '//localhost:3030/api/'
+    : '//localhost:3000/api/'
 
 
 import Axios from 'axios';
